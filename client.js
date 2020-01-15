f05efe7a78084deadb5a25323a999581059aee48
Tyler <yiqizhang@gmail.com>
2020-01-15 19:14:46 +0000

    client updated

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "172.46.2.204",
+    port: 50541
+  });
+  conn.on("data", data => {
+    console.log("It's got a sharp tongue, this server!", data);
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
